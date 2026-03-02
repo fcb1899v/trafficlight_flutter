82990e39a271005d9716b0434b45e99800b1c413
nakajimamasao <chelseav3@hotmail.com>
2026-03-02 21:17:31 +0900

    Updated all other dependencies to the latest.

--- android/app/src/main/kotlin/nakajimamasao/appstudio/signalbutton/MainActivity.kt
+++ android/app/src/main/kotlin/nakajimamasao/appstudio/signalbutton/MainActivity.kt
@@ -1,6 +1,20 @@
 package nakajimamasao.appstudio.signalbutton
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && Build.VERSION.SDK_INT < 35) {
+            @Suppress("DEPRECATION")
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+            @Suppress("DEPRECATION")
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+        }
+    }
 }
